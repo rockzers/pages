bbb8f6bcdff7a12b08f89af1d7393489210899a3
rinmishima <saivinodhini26@gmail.com>
2017-01-13 19:20:12 +0700

    form table create,show,flash msg in template

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,10 +3,17 @@ def new
   @article = Article.new
 end
 def create
-  #render plain: params[:article].inspect
   @article = Article.new(article_params)
-  @article.save
-end
+   if @article.save
+     flash[:notice] = "Article created  successfully"
+     redirect_to article_path(@article)
+   else
+      render 'new'
+   end
+ end
+ def show
+ @article = Article.find(params[:id])
+ end
  private
   def article_params
     params.require(:article).permit(:title, :description)
