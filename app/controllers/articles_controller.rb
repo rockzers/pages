d694130e23909d29f31262c1c4e4a8461640fbc3
rinmishima <saivinodhini26@gmail.com>
2017-01-20 23:03:29 +0700

    add particles and destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -30,6 +30,13 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "destroy  successfully"
+    redirect_to articles_path
+  end
   private
   def article_params
     params.require(:article).permit(:title, :description)
