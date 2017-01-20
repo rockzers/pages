c5fdf8f605deb777a3f9b9cdc837b1c8ad18b575
rinmishima <saivinodhini26@gmail.com>
2017-01-20 22:27:03 +0700

    add edit delete html listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,12 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles=Article.all
+  end
   def new
     @article = Article.new
+  end
+  def edit
+    @article = Article.find(params[:id])
   end
   def create
     @article = Article.new(article_params)
@@ -10,6 +16,16 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
+  end
+  def update
+      @article = Article.find(params[:id])
+      if @article.update(article_params)
+        flash[:notice] = "update  successfully"
+        redirect_to article_path(@article)
+      else
+         render 'edit'
+      end
+
   end
   def show
     @article = Article.find(params[:id])
