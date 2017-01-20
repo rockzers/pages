9940f457968b7a6fadeebf6991f2beecbec9a033
rinmishima <saivinodhini26@gmail.com>
2017-01-20 21:15:10 +0700

    fix create article and show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,20 @@
 class ArticlesController < ApplicationController
-def new
-  @article = Article.new
-end
-def create
-  @article = Article.new(article_params)
-   if @article.save
+  def new
+    @article = Article.new
+  end
+  def create
+    @article = Article.new(article_params)
+    if @article.save
      flash[:notice] = "Article created  successfully"
      redirect_to article_path(@article)
-   else
+    else
       render 'new'
-   end
- end
- def show
- @article = Article.find(params[:id])
- end
- private
+    end
+  end
+  def show
+    @article = Article.find(params[:id])
+  end
+  private
   def article_params
     params.require(:article).permit(:title, :description)
   end
