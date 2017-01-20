529eadf6a05d3eb8cc2d54c04936afa73300ccdd
rinmishima <saivinodhini26@gmail.com>
2017-01-20 23:15:39 +0700

    before action method use controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit,:update,:show,:destroy]
   def index
     @articles=Article.all
   end
@@ -6,7 +7,7 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   def edit
-    @article = Article.find(params[:id])
+
   end
   def create
     @article = Article.new(article_params)
@@ -18,7 +19,7 @@ class ArticlesController < ApplicationController
     end
   end
   def update
-      @article = Article.find(params[:id])
+
       if @article.update(article_params)
         flash[:notice] = "update  successfully"
         redirect_to article_path(@article)
@@ -28,16 +29,19 @@ class ArticlesController < ApplicationController
 
   end
   def show
-    @article = Article.find(params[:id])
+
   end
-  
+
   def destroy
-    @article = Article.find(params[:id])
+
     @article.destroy
     flash[:notice] = "destroy  successfully"
     redirect_to articles_path
   end
   private
+  def set_article
+    @article = Article.find(params[:id])
+  end
   def article_params
     params.require(:article).permit(:title, :description)
   end
